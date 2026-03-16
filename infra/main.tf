3eae55180a1819e21dad0a6c50ee42cde4b5a424
lupucci <lupucci05@gmail.com>
2026-03-16 15:35:59 -0300

    change public ip sku to standard

--- infra/main.tf
+++ infra/main.tf
@@ -96,9 +96,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   source_image_reference {
-  publisher = "Canonical"
-  offer     = "0001-com-ubuntu-server-jammy"
-  sku       = "22_04-lts"
-  version   = "latest"
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
   }
 }
