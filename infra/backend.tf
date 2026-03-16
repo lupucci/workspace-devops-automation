5abfd63000f2aebd52a356a4692491e8bc86bec1
lupucci <lupucci05@gmail.com>
2026-03-16 14:24:16 -0300

    fix terraform formatting

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name = "rg-tfstate"
-        storage_account_name = "tfstate32711"
-        container_name = "tfstate"
-        key = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstate32711"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
